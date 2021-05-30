614adb8d9cb13acbea021ba469a8268deb96e738
Faris Durrani <afarisdurrani@gmail.com>
2021-05-30 12:55:56 -0400

    drafr 2.4

--- project_2_4/project_2_4.cpp
+++ project_2_4/project_2_4.cpp
@@ -1,7 +1,8 @@
-#include <iostream>  // Gives us access to std::cin and std::cout
-#include <string>  // Gives us access to std::string and std::getline()
+#include <iostream> // Gives us access to std::cin and std::cout
+#include <string>   // Gives us access to std::string and std::getline()
 
-struct LightOutputs {
+struct LightOutputs
+{
   bool red_ns;
   bool yellow_ns;
   bool green_ns;
@@ -10,29 +11,144 @@ struct LightOutputs {
   bool green_ew;
 };
 
-struct LightInputs {
+struct LightInputs
+{
   int time;
   bool car_sensor_n;
   bool car_sensor_s;
 };
 
 // TODO Define your light controller state machine class here
+LightOutputs out;
+LightInputs in;
+
+bool noCarsNS(LightInputs in)
+{
+  return in.car_sensor_n && in.car_sensor_s;
+}
+
+bool bothRed(LightOutputs out)
+{
+  return out.red_ew && out.red_ns;
+}
 
 int main()
 {
-    // TODO Initialize your state machine class here
-    while(true)
+  // TODO Initialize your state machine class here
+  out.red_ns = 1;
+  out.yellow_ns = 0;
+  out.green_ns = 0;
+
+  out.red_ew = 1;
+  out.yellow_ew = 0;
+  out.green_ew = 0;
+
+  in.time = 0;
+
+  while (true)
+  {
+    std::string input;
+    std::getline(std::cin, input);
+    if (input.size() != 2)
+      break;
+
+    // TODO Parse the input digits
+    in.car_sensor_n = std::stoi(input.substr(0, 1));
+    in.car_sensor_s = std::stoi(input.substr(1, 1));
+
+    // TODO Update your state machine based on input
+    if (bothRed(out))
     {
-        std::string input;
-        std::getline(std::cin, input);
-        if(input.size() != 2)
-            break;
-
-        // TODO Parse the input digits
-
-        // TODO Update your state machine based on input
-
-        // TODO Output your state machine's light controls to std::cout
+      // if both red
+      if (in.time < 1)
+      {
+        // if both red and not yet one second, do nothing
+      }
+      else
+      {
+        // if been one second, change to green
+        in.time = 0;
+        if (noCarsNS)
+        {
+          out.red_ew = 0;
+          out.green_ew = 1;
+        }
+        else
+        {
+          out.red_ns = 0;
+          out.green_ns = 1;
+        }
+      }
     }
-    return 0;
+    else if (out.red_ew)
+    {
+      // if NS is moving
+      if (out.yellow_ns)
+      {
+        // if NS is yellow
+        if (in.time < 2)
+        {
+          // do nothing
+        }
+        else
+        {
+          out.yellow_ns = 0;
+          out.red_ns = 1;
+          in.time = 0;
+        }
+      }
+      else
+      {
+        // if NS is green
+        if (in.time < 5)
+        {
+          // do nothing
+        }
+        else
+        {
+          out.green_ns = 0;
+          out.yellow_ns = 1;
+          in.time = 0;
+        }
+      }
+    }
+    else
+    {
+      // if EW is moving
+      if (out.yellow_ew)
+      {
+        // if EW is yellow
+        if (in.time < 2)
+        {
+          // do nothing
+        }
+        else
+        {
+          out.yellow_ew = 0;
+          out.red_ew = 1;
+          in.time = 0;
+        }
+      }
+      else
+      {
+        // if EW is green
+        if (in.time < 5)
+        {
+          // do nothing
+        }
+        else
+        {
+          out.green_ew = 0;
+          out.yellow_ew = 1;
+          in.time = 0;
+        }
+      }
+    }
+
+    in.time++;
+
+    // TODO Output your state machine's light controls to std::cout
+    std::cout << out.red_ns << out.yellow_ns << out.green_ns << out.red_ew << out.yellow_ew << out.green_ew << std::endl;
+  }
+  return 0;
 }
